b78835b1feb74038a4b5d3f80815e9be107f0457
Eugene Dzhurinsky <jdevelop@gmail.com>
2013-06-30 19:14:10 -0400

    Use Apache Httpclient for OAUth

--- src/main/scala/com/squeakybeaker/order/authentication/OAuth2Google.scala
+++ src/main/scala/com/squeakybeaker/order/authentication/OAuth2Google.scala
@@ -2,9 +2,16 @@ package com.squeakybeaker.order.authentication
 
 import net.liftweb.json.JsonAST.JObject
 import org.slf4j.LoggerFactory
-import com.squeakybeaker.order.model.{Entity, DAO}
+import com.squeakybeaker.order.model.DAO
 import com.squeakybeaker.order.model.Entity.User
 import com.squeakybeaker.order.lib.DBAware
+import org.apache.http.impl.conn.PoolingClientConnectionManager
+import org.apache.http.client.HttpClient
+import org.apache.http.impl.client.DefaultHttpClient
+import org.apache.http.HttpStatus
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.client.entity.UrlEncodedFormEntity
+import org.apache.http.message.BasicNameValuePair
 
 /**
  * User: Eugene Dzhurinsky
@@ -42,16 +49,19 @@ class OAuth2Google(dao: DAO) extends OAuth2 with DBAware {
 
 object OAuth2Google {
 
+  private val poolingConnMgr = new PoolingClientConnectionManager()
+
+  private val httpClient: HttpClient = new DefaultHttpClient(poolingConnMgr)
+
   val LOG = LoggerFactory.getLogger(classOf[OAuth2Google])
 
   case class GoogleConfig(redirect: String, cid: String, secret: String)
 
   def requestAccessToken(c: GoogleConfig)(token: String): Option[String] = {
-    import scalaj.http._
     LOG.debug("Getting access token: {}", token)
+    var request : HttpPost = null
     try {
       val url = "https://accounts.google.com/o/oauth2/token"
-      LOG.debug("Sending request to {}", url)
       val prms = List(
         "code" -> token,
         "redirect_uri" -> c.redirect,
@@ -60,15 +70,38 @@ object OAuth2Google {
         "grant_type" -> "authorization_code"
       )
       LOG.debug("Sending request to {} => {}", url, prms)
-      val response = Http.post(url).params(prms).asString
-      LOG.debug("Oauth response: {}", response)
-      import net.liftweb.json.JsonParser._
-      val mapVals = parse(response).asInstanceOf[JObject].values
-      mapVals.get("access_token").map(_.toString)
+      import collection.JavaConverters._
+
+      val nvps = prms.map {
+        case (n, v) => new BasicNameValuePair(n, v)
+      }.asJava
+
+      request = new HttpPost(url)
+
+      request.setEntity(new UrlEncodedFormEntity(nvps, "UTF-8"))
+
+      val response = httpClient.execute(request)
+
+      val content = response.getStatusLine.getStatusCode match {
+        case HttpStatus.SC_OK => Some(response.getEntity.getContent)
+        case _ =>
+          LOG.error("Request resulted in ", io.Source.fromInputStream(response.getEntity.getContent).mkString)
+          None
+      }
+      content.flatMap {
+        v =>
+          val oauthResp = io.Source.fromInputStream(v).mkString
+          LOG.debug("Oauth response: {}", oauthResp)
+          import net.liftweb.json.JsonParser._
+          val mapVals = parse(oauthResp).asInstanceOf[JObject].values
+          mapVals.get("access_token").map(_.toString)
+      }
     } catch {
       case e: Exception =>
         LOG.error("Can't retrieve oauth token", e)
         None
+    } finally {
+      Option(request).foreach(_.abort())
     }
   }
 
